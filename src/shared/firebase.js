d58bf2dd250aba4f8311cf788e87b3d4fc935415
Lewis <lewis57350@gmail.com>
2025-09-27 12:09:41 -0500

    Fix firebase init: dedupe getFirestore and unify imports

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -6,7 +6,6 @@ import {
   setPersistence,
   browserLocalPersistence,
 } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
